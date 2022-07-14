e63785ed9f08ac2fa4f87ff1e6e10bae8d14d982
Marsel Gaisin <marselgaisin@gmail.com>
2022-07-14 16:24:06 +0300

    office

---
+++ src/main/java/org/spring_introduction/Test3.java
@@ -0,0 +1,16 @@
+package org.spring_introduction;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test3 {
+    public static void main(String[] args) {
+        //Создаем спринг контейнер
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("aplicationContext.xml");
+        Pet pet = context.getBean("myPet", Pet.class);
+
+        Person person = new Person(pet);
+        person.callYourPet();
+
+        context.close();
+    }
+}
